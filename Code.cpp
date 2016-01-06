fd293839af92243ad77fcd274e15004ee1a5bd83
NSuri1 <nalinsuri@gmail.com>
2016-01-05 21:05:14 -0500

    Test with One Array

--- Code.cpp
+++ Code.cpp
@@ -7,32 +7,35 @@ using namespace std;
 const int SIZE=6;
 const int NUMSIZE=5;
 	
-void copyData(string [], int [][NUMSIZE]);
-void displayArrays();
+void copyData(string (&names)[SIZE], int (&numbers)[SIZE][NUMSIZE]);
+void displayArrays(string [], int [][NUMSIZE]);
 
-int main() {
+int main() 
+{
 	
 
 	string names[SIZE];
 	int numbers[SIZE][NUMSIZE];
 	
 	copyData(names, numbers);
+	displayArrays(names, numbers);
 	
 	getch();
 	return 0;
 }
 
-void copyData(string names[], int numbers[][NUMSIZE])
+void copyData(string (&names)[SIZE], int (&numbers)[SIZE][NUMSIZE])
 {
+	int count= 0;
 	ifstream inputFile;
 	inputFile.open("dayOne.txt");
 	
 	if(inputFile)
 	{
-		while(!inputFile.eof())
+		while(count<SIZE && !inputFile.eof())
 		{
-			//inputFile>>
-			//You need to finish this line of code
+			inputFile>> names[count];
+			count++;
 		}	
 		inputFile.close(); 
 	}
@@ -41,5 +44,14 @@ void copyData(string names[], int numbers[][NUMSIZE])
 		cout<< "Error opening file.\n";
 	}
 }
+
+void displayArrays(string names[], int numbers[][NUMSIZE])
+{
+	for(int count=0; count<SIZE; count++)
+	{
+		cout<< names[count]<< endl;
+	}
+}
+
 
 
